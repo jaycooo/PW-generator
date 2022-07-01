7a3c1e51ad1928ca65a17255a4659c2517fc6cb9
jaycooo <j.coronado@live.com>
2022-07-01 14:42:32 -0400

    updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var playername = window.prompt("How many characters do you want your password to be? \nPassword must be between 8 and 128 characters");
+var password = window.prompt("How many characters do you want your password to be? \nPassword must be between 8 and 128 characters");
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // creating password object.
 var pwdCriteria = {
@@ -21,13 +22,26 @@ var pwdCriteria = {
 
   // append child array specials
   pwdCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
-  "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]//
+  "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]//32
 }
- //variables to collect input from user prompts
- var passwordLength = 0;
- var upperCase;
- var lowerCase;
- var numbers;
- var specialChar;
+function writePassword() {
+  //call password function
+  var password = generatePassword();
 
- generateBtn.addEventListener("click", pwdLowerCase);
+  //show password in in box
+  var passwordText =document.querySelector("#password");
+
+  //update the textArea with the new password 
+  passwordText.value = password;
+}
+ 
+// event listener
+generateBtn.addEventListener("click", writePassword);
+
+//Function to generate password
+function generatePassword() {
+  //result holder
+  var result ="";
+
+  //Holder for user input
+}
